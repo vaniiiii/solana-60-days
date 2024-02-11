52cea3b5ba741b865a3d74a9f6fdc9d7983a260e
Ivan <57509528+vaniiiii@users.noreply.github.com>
2024-02-11 11:36:50 +0100

    day_3 added

---
+++ day_3/tests/day_3.ts
@@ -0,0 +1,17 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Day3 } from "../target/types/day_3";
+
+describe("day_3", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.Day3 as Program<Day3>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods
+      .initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
