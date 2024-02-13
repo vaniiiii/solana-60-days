1e86d300a2d71dcbb5f59da447229e39f4a9c087
Ivan <57509528+vaniiiii@users.noreply.github.com>
2024-02-14 00:02:08 +0100

    day_4 added

--- day_3/programs/day_3/src/lib.rs
+++ day_3/programs/day_3/src/lib.rs
@@ -1,20 +1,36 @@
 use anchor_lang::prelude::*;
 
-declare_id!("8PSAL9t1RMb7BcewhsSFrRQDq61Y7YXC5kHUxMk5b39Z");
+declare_id!("4YP3z3EGEUakijwxJTd2P912PxaAxGFTWJsd9zjbPgcm");
 
 #[program]
-pub mod anchor_function_tutorial {
+pub mod day_3 {
     use super::*;
 
-    pub fn function_a(ctx: Context<NonEmptyAccountExample>) -> Result<()> {
+    pub fn function_a(_ctx: Context<NonEmptyAccountExample>) -> Result<()> {
         Ok(())
     }
 
-    pub fn function_b(ctx: Context<Empty>, firstArg: u64) -> Result<()> {
+    pub fn function_b(_ctx: Context<Empty>, _first_arg: u64) -> Result<()> {
         Ok(())
+    }
+
+    pub fn limit_range(_ctx: Context<LimitRange>, a: u64) -> Result<()> {
+        if a < 10 {
+            return err!(MyError::AisToSmall);
+        }
+        if a > 100 {
+            return err!(MyError::AisToBig);
+        }
+
+        msg!("Result = {}", a);
+        Ok(())
+    }
+
+    pub fn func(ctx: Context<ReturnError>) -> Result<()> {
+        msg!("Will this print?");
+        return err!(Day4Error::AlwaysErrors);
     }
 }
-
 #[derive(Accounts)]
 pub struct NonEmptyAccountExample<'info> {
     signer: Signer<'info>,
@@ -23,3 +39,23 @@ pub struct NonEmptyAccountExample<'info> {
 
 #[derive(Accounts)]
 pub struct Empty {}
+
+#[derive(Accounts)]
+pub struct LimitRange {}
+
+#[derive(Accounts)]
+pub struct ReturnError {}
+
+#[error_code]
+pub enum MyError {
+    #[msg("a is too small")]
+    AisToSmall,
+    #[msg("a is too big")]
+    AisToBig,
+}
+
+#[error_code]
+pub enum Day4Error {
+    #[msg("Always errors")]
+    AlwaysErrors,
+}
--- day_3/tests/day_3.ts
+++ day_3/tests/day_3.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import { Program, AnchorError } from "@coral-xyz/anchor";
 import { Day3 } from "../target/types/day_3";
+import { assert } from "chai";
 
 describe("day_3", () => {
   // Configure the client to use the local cluster.
@@ -11,7 +12,54 @@ describe("day_3", () => {
   it("Is initialized!", async () => {
     // Add your test here.
     const tx = await program.methods
-      .initialize().rpc();
+      .functionA().rpc();
     console.log("Your transaction signature", tx);
+  });
+});
+describe("day4", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.Day3 as Program<Day3>;
+
+  it("Input test", async () => {
+    // Add your test here.
+    try {
+      const tx = await program.methods.limitRange(new anchor.BN(9)).rpc();
+      console.log("Your transaction signature", tx);
+    } catch (_err) {
+      assert.isTrue(_err instanceof AnchorError);
+      const err: AnchorError = _err;
+      const errMsg =
+        "a is too small";
+      assert.strictEqual(err.error.errorMessage, errMsg);
+      console.log("Error number:", err.error.errorCode.number);
+    }
+
+    try {
+      const tx = await program.methods.limitRange(new anchor.BN(101)).rpc();
+      console.log("Your transaction signature", tx);
+    } catch (_err) {
+      assert.isTrue(_err instanceof AnchorError);
+      const err: AnchorError = _err;
+      const errMsg =
+        "a is too big";
+      assert.strictEqual(err.error.errorMessage, errMsg);
+      console.log("Error number:", err.error.errorCode.number);
+    }
+  });
+  it("Error test", async () => {
+    // Add your test here.
+    try {
+      const tx = await program.methods.func().rpc();
+      console.log("Your transaction signature", tx);
+    } catch (_err) {
+      assert.isTrue(_err instanceof AnchorError);
+      const err: AnchorError = _err;
+      const errMsg =
+        "Always errors";
+      assert.strictEqual(err.error.errorMessage, errMsg);
+      console.log("Error number:", err.error.errorCode.number);
+    }
   });
 });
